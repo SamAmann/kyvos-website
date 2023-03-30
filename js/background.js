787c9feff4642520115d02800b2ddf212021e0e0
Samuel Amann <samuel.amann@kyvos.me>
2023-03-30 11:24:44 +0200

    Flexibility and CUstomisations
    Adds working navButton (.active when clicked)
    Continues construction of container

--- js/background.js
+++ js/background.js
@@ -1,13 +1,23 @@
 // To navigate between pages
-function togglePage(id) {
+function navToPage(id) {
     var pages = document.getElementsByTagName("page");
+    var btnNav = document.getElementsByClassName("btn-nav");
     for (var i = 0; i < pages.length; i++) {
-        if (pages[i].id == id) {
+        if (pages[i].id == `page-${id}`) {
             pages[i].classList.add("active");
             pages[i].classList.remove("inactive");
         } else {
             pages[i].classList.add("inactive");
             pages[i].classList.remove("active");
+        }
+    }
+    for (var i = 0; i < btnNav.length; i++) {
+        if (btnNav[i].id == "btn-" + id) {
+            btnNav[i].classList.add("active");
+            btnNav[i].classList.remove("inactive");
+        } else {
+            btnNav[i].classList.add("inactive");
+            btnNav[i].classList.remove("active");
         }
     }
 }
