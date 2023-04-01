b89732347f81db817ee3d308a92397fa7a5ea084
Samuel Amann <samuel.amann@kyvos.me>
2023-04-01 11:38:06 +0200

    Update Responsive
    Creates the nav for mobile
    Improve containers and contents selectors for mobile

--- js/background.js
+++ js/background.js
@@ -1,7 +1,23 @@
 // To navigate between pages
+
+
+
+
 function navToPage(id) {
+    var clickedBtn = document.getElementById("btn-" + id);
+
     var pages = document.getElementsByTagName("page");
     var btnNav = document.getElementsByClassName("btn-nav");
+
+    if (!clickedBtn) {
+        console.error(`Element with ID 'btn-${id}' not found.`);
+        return;
+    }
+
+    if (clickedBtn.classList.contains('disabled')) {
+        return;
+    }
+
     for (var i = 0; i < pages.length; i++) {
         if (pages[i].id == `page-${id}`) {
             pages[i].classList.add("active");
@@ -43,5 +59,41 @@ function scrollToElement(id) {
         behavior: 'smooth'
     })
 }
+
+// Mobile menu management
+const btnMobileMenu = document.getElementById("btn-mobile-menu");
+const mobileMenu = document.querySelector("nav");
+const menuIcon = document.querySelector('#btn-mobile-menu img');
+
+btnMobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("open");
+    if (mobileMenu.classList.contains("open")) {
+        menuIcon.src = "../img/icons/cross.svg";
+        btnMobileMenu.focus();
+    } else {
+        menuIcon.src = "../img/icons/burger-menu.svg";
+    }
+});
+
+function closeMobileMenu() {
+    mobileMenu.classList.remove("open");
+    menuIcon.src = "../img/icons/burger-menu.svg";
+}
+
+// Closes mobile menu when user clicks outside of the menu
+document.addEventListener("mousedown", function (event) {
+    if (!mobileMenu.contains(event.target) && !btnMobileMenu.contains(event.target)) {
+        closeMobileMenu();
+    }
+});
+
+const btnNavElements = document.querySelectorAll(".btn-nav");
+
+btnNavElements.forEach(btn => {
+    btn.addEventListener("click", function () {
+        closeMobileMenu();
+    });
+});
+
 
 
